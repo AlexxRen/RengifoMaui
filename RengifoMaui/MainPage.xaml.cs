09759b62e44f265d14d4d496d39eff91da32daa2
Alexander <alexren567@outlook.com>
2024-06-03 08:45:00 -0500

    crencion vista venta del sistema recarga

--- RengifoMaui/MainPage.xaml.cs
+++ RengifoMaui/MainPage.xaml.cs
@@ -9,17 +9,20 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = input_telf.Text;
         }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+
+
     }
 
 }
