38bfc94d8155d554c4b6255b3227490ea5a909c0
Alexander <alexren567@outlook.com>
2024-06-03 07:35:48 -0500

    Creacion de Modelo venta con companias quemadas en una lista.

---
+++ RengifoMaui/Resources/Modelos/RAVenta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RengifoMaui.Resources.Modelos
+{
+    internal class RAVenta
+    {
+        //variables
+        public string ? Nombres { get; set; }
+        public int ? Telefono { get; set; }
+        public List<string> Companias = new List<string>() { "Movistar", "Claro", "Tuenti" };
+        public string ? Operador { get; set; }
+        
+        
+        //constructores
+        public RAVenta() 
+        
+        {
+            Nombres = null;
+            Telefono =0;
+            this.Operador = null;
+        }
+
+        public RAVenta( string _Nombres,int _Telefono,string _Operador) 
+        { 
+           this.Nombres = _Nombres;
+            this.Telefono = _Telefono;
+            this.Operador = _Operador;
+        }
+        // metodos
+
+
+    }
+}
