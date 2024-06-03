a28f53bfe22b7fcd4a095414f3dcc8799f622230
laboratorio <laboratorio@UPEAULA-31724>
2024-06-03 07:10:38 -0500

    Agregar archivos de proyecto.

---
+++ RengifoMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RengifoMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ RengifoMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RengifoMaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
